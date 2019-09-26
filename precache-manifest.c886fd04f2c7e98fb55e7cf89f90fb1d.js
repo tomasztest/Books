fdc316d526e166c3b2ea294f2beefcf5d0db322d
tomasztest <tomaszkotzur@gmail.com>
2019-09-26 09:05:50 +0200

    Updates

---
+++ precache-manifest.c886fd04f2c7e98fb55e7cf89f90fb1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dca0ec4309cb67f7cf8b42a45ca779",
+    "url": "/Books/index.html"
+  },
+  {
+    "revision": "336083c965cef52e387b",
+    "url": "/Books/static/css/main.c365bd7e.chunk.css"
+  },
+  {
+    "revision": "5b8c6977c47ecb655f21",
+    "url": "/Books/static/js/2.5542a73c.chunk.js"
+  },
+  {
+    "revision": "336083c965cef52e387b",
+    "url": "/Books/static/js/main.19a4fc5f.chunk.js"
+  },
+  {
+    "revision": "db1e26ccd1b1db44586b",
+    "url": "/Books/static/js/runtime-main.74530925.js"
+  }
+]);
